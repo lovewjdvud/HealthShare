7c96c68e25f50d46599e562052c17b0ad709f95e
lovewjdvud <76860314+lovewjdvud@users.noreply.github.com>
2022-12-24 15:55:45 +0900

    1224

---
+++ HealthShare/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthShare
+//
+//  Created by Songjeongpyeong on 2022/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
